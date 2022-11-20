4cb96ffd78aab68acc634a97cd97f8800e5c9ea1
Andrew Litvinov <lita.private@gmail.com>
2022-11-20 16:51:43 +0300

    feat: task fs:read done

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,19 @@
+import { readFile } from 'node:fs/promises';
+import { join, dirname } from 'node:path';
+import { fileURLToPath } from 'node:url';
+
+import { debugMsg, usersError } from '../userUtlis.js';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+const targetFileURL = join(__dirname, 'files', 'fileToRead.txt');
+
 const read = async () => {
-    // Write your code here 
+  try {
+    const text = await readFile(targetFileURL, { encoding: 'utf8' });
+    console.log(text);
+  } catch (error) {
+    usersError(' Error reading file');
+  }
 };
 
 await read();
