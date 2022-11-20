bd0e7247a099615b145bab39d4767a4381a4cea2
Andrew Litvinov <lita.private@gmail.com>
2022-11-20 20:08:37 +0300

    feat: task cli:env done

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,19 @@
+import process from 'node:process';
 const parseEnv = () => {
-    // Write your code here 
+  let outputText = '';
+  const arrayKeysOfEnvVar = Object.keys(process.env);
+
+  const arrayKeysWithRSS_ = arrayKeysOfEnvVar.filter((el) =>
+    el.startsWith('RSS_')
+  );
+
+  arrayKeysWithRSS_.forEach(
+    (el) =>
+      (outputText += `${el}=${process.env[el]}${
+        arrayKeysWithRSS_[arrayKeysWithRSS_.length - 1] === el ? '' : '; '
+      }`)
+  );
+  console.log(outputText);
 };
 
 parseEnv();
