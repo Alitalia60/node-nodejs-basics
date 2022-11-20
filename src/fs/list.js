07cc1de97ea7677a14d77ced90c11f314e404ab8
Andrew Litvinov <lita.private@gmail.com>
2022-11-20 16:45:49 +0300

    feat: task fs:list done

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,23 @@
+import { readdir, stat } from 'node:fs/promises';
+import path, { join, dirname } from 'node:path';
+import { fileURLToPath } from 'node:url';
+
+import { debugMsg, usersError } from '../userUtlis.js';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+const sourceDirURL = join(__dirname, 'files');
+
 const list = async () => {
-    // Write your code here 
+  // Write your code here
+  const files = await readdir(sourceDirURL).catch((err) =>
+    usersError(err.code)
+  );
+  if (!files.length) {
+    usersError(' Source dir is empty');
+  }
+
+  console.table(files);
+  debugMsg(' File names printed');
 };
 
 await list();
