47a15c01968bec10b02dce1498e381a7263be7d5
Andrew Litvinov <lita.study@bk.ru>
2022-11-21 16:25:57 +0300

    refactor: update code task cli:args

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,12 +1,18 @@
 const parseArgs = () => {
-  // Write your code here
+  let arrayArgs = process.argv.slice(2);
+  let arrayPairs = [];
 
-  let message = process.argv.slice(2).toString();
-  message = message.replaceAll(',--', ';');
-  message = message.replaceAll('--', '');
-  message = message.replaceAll(',', ' is ');
-  message = message.replaceAll(';', ', ');
-  console.log(message);
+  for (let index = 0; index < arrayArgs.length; index++) {
+    const element = arrayArgs[index];
+    if (element.startsWith('--')) {
+      if (!arrayArgs[index + 1].startsWith('--')) {
+        arrayPairs.push(
+          ` ${element.replace('--', '')} is ${arrayArgs[index + 1]}`
+        );
+      }
+    }
+  }
+  console.log(arrayPairs.toString());
 };
 
 parseArgs();
