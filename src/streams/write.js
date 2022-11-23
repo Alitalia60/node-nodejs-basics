69af8a092682e89d3b1a56926e79cdd88a4d6912
Andrew Litvinov <lita.study@bk.ru>
2022-11-23 17:53:43 +0300

    feat: tasks stream done

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,19 @@
+import { createWriteStream } from 'node:fs';
+import { dirname, join } from 'node:path';
+import { stdin } from 'node:process';
+import { fileURLToPath } from 'node:url';
+
+import { usersError } from '../userUtlis.js';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+const fileToReadURL = join(__dirname, 'files', 'fileToWrite.txt');
+
 const write = async () => {
-    // Write your code here 
+  const output = createWriteStream(fileToReadURL, { encoding: 'utf-8' });
+  output
+    .on('error', (err) => usersError(err.code))
+    .on('finish', () => output.destroy());
+  stdin.pipe(output);
 };
 
 await write();
