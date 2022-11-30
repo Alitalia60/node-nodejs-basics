ce71b7af0967e4cec395c7aefb5e942be47a5cd6
Andrew Litvinov <lita.private@gmail.com>
2022-11-30 22:09:36 +0300

    refactor: join to another workplace

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,21 @@
+import { cpus } from 'node:os';
+import { Worker, isMainThread } from 'node:worker_threads';
+import { dirname, join } from 'node:path';
+import { fileURLToPath } from 'node:url';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+console.log(__dirname);
+const cpusCounter = cpus().length;
+
 const performCalculations = async () => {
-    // Write your code here
+  // Write your code here
+
+  if (isMainThread) {
+  }
+  for (let index = 0; index < cpusCounter; index++) {
+    // run worker;
+    const worker = new Worker(join(__dirname, 'worker.js'));
+  }
 };
 
 await performCalculations();
