3f5e10bc8a72b31b5da0c4b4c26b122c113ebcde
Andrew Litvinov <lita.private@gmail.com>
2022-12-03 22:18:48 +0300

    feat: task wt done

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,7 +1,7 @@
 import { Worker, isMainThread } from 'node:worker_threads';
 import { cpus, userInfo } from 'node:os';
 import { fileURLToPath } from 'node:url';
-import { dirname, join } from 'node:path';
+import { dirname, join, resolve } from 'node:path';
 
 import { usersError } from '../userUtlis.js';
 
@@ -9,15 +9,27 @@ const __dirname = dirname(fileURLToPath(import.meta.url));
 const workerURL = join(__dirname, './worker.js');
 
 const performCalculations = async () => {
-  const arrResult = [];
-  const arrPromises = [];
-  for (let index = 0; index < cpus().length; index++) {
-    const work = new Worker(workerURL, { workerData: 10 + index });
-    work.on('message', (mes) => {
-      arrResult.push(mes);
-    });
+  if (isMainThread) {
+    const arrPromises = [];
+    for (let index = 0; index < cpus().length; index++) {
+      arrPromises.push(createPromise(index));
+    }
+    try {
+      await Promise.all(arrPromises).then((result) => {
+        console.log(result);
+      });
+    } catch (error) {
+      usersError(error);
+    }
   }
-  console.log(arrResult);
 };
+
+function createPromise(index) {
+  return new Promise((resolve, reject) => {
+    const work = new Worker(workerURL, { workerData: 10 + index });
+    work.on('message', (mes) => resolve(mes));
+    work.on('error', (err) => reject(err));
+  });
+}
 
 await performCalculations();
--- src/wt/worker.js
+++ src/wt/worker.js
@@ -4,10 +4,7 @@ const nthFibonacci = (n) =>
   n < 2 ? n : nthFibonacci(n - 1) + nthFibonacci(n - 2);
 
 const sendResult = () => {
-  console.log('fibo=', nthFibonacci(workerData));
   parentPort.postMessage(nthFibonacci(workerData));
-  // parentPort.on('message', () => console.log('from main ' + workerData));
-  // This function sends result of nthFibonacci computations to main thread
 };
 
 sendResult();
